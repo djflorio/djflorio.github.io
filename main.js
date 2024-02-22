911f1a0439fc6cc452ed38b9b4faca25b507bf33
djflorio <dflo@danflorio.com>
2024-02-22 10:52:05 -0500

    experiment with animations

--- main.js
+++ main.js
@@ -5,21 +5,21 @@ generate();
 function generate() {
   svg.clear();
 
-  for (let i = 0; i < 20; i++) {
-    const circle = svg.circle(32).cx(100).cy(100).fill("hsl(0, 100%, 100%)").node;
+  for (let i = 0; i < 75; i++) {
+    const circle = svg.circle(5).cx(100).cy(100).fill("hsl(0, 100%, 100%)").node;
 
     gsap.to(circle, {
       x: "random(-100, 100, true)",
       y: "random(-100, 100, true)",
       scale: "random(0.25, 1, true)",
-      ease: "elastic.out(1, 0.5)",
-      duration: 1.5,
-      delay: "random(0, 0.2, true)",
+      ease: "power3.out(1, 0.5)",
+      duration: 3,
+      delay: "random(0, 0.6, true)",
       attr: {
-        fill: () => `hsl(${gsap.utils.random(0, 360)}, 75%, 75%)`
+        fill: () => `hsl(103, 50%, ${gsap.utils.random(40, 75)}%)`
       },
       repeat: -1,
-      repeatDelay: 1.5,
+      repeatDelay: 0,
       repeatRefresh: true
     });
   }
