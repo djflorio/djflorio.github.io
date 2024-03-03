f00c5b1e5fb6447a7e9dbfb35404181943ebda93
Dan Florio <dflo@danflorio.com>
2024-03-02 19:17:37 -0500

    start adding new animations

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ generate();
 
 function generate() {
   musicSvg.clear();
+  codingSvg.clear();
 
   for (let i = 0; i < 75; i++) {
     const circle = musicSvg.circle(5).cx(100).cy(100).fill("hsl(0, 100%, 100%)").node;
@@ -24,5 +25,22 @@ function generate() {
       repeatDelay: 0,
       repeatRefresh: true
     });
+  }
+
+  for (let i = 0; i < 75; i++) {
+    const circle = codingSvg.circle(5).cx(50).cy(100).fill("hsl(0, 100%, 100%)").node;
+
+    /*gsap.to(circle, {
+      y: -100,
+      scale: "random(0.25, 1, true)",
+      duration: 3,
+      delay: "random(0, 0.6, true)",
+      attr: {
+        fill: () => `hsl(53, 50%, ${gsap.utils.random(40, 75)}%)`
+      },
+      repeat: -1,
+      repeatDelay: 0,
+      repeatRefresh: true
+    });*/
   }
 }
